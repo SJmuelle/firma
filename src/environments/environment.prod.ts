44852ffe6f7fa7fa66afa08b13cf67807123e90a
tavosalas27 <tavosalas270@gmail.com>
2022-08-23 09:05:13 -0500

    Actualizados los environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     apiPath:"http://demo.fintra.co:8011/",
+    apiPrueba:"https://7db0-181-57-229-82.ngrok.io/",
     userName: "APPWEB",
     password: "123456",
 };
