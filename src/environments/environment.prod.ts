09e34b90f797f924bff22a8a2e046feccb479c05
tavosalas27 <tavosalas270@gmail.com>
2022-09-12 08:43:33 -0500

    Actualizado ngrok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     apiPath:"http://demo.fintra.co:8011/",
-    apiPrueba:"https://27ef-181-57-229-82.ngrok.io/",
+    apiPrueba:"https://a4ad-181-57-229-82.ngrok.io/",
     userName: "APPWEB",
     password: "123456",
 };
