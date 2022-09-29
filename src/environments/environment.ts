c668a88c88823bb471cff516883959441f472ccd
tavosalas27 <tavosalas270@gmail.com>
2022-09-29 16:30:03 -0500

    Corregido el environmet para prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
     production: false,
-    apiPath:"https://88dd-181-57-229-82.ngrok.io/",
-    // apiPath: 'https://prometheus.fintra.co:8443/',
+    //apiPath:"https://88dd-181-57-229-82.ngrok.io/",
+    apiPath: 'https://prometheus.fintra.co:8443/',
 
     userName: "APPWEB",
     password: "123456",
