ce5a3da8f84363ef00c1b88b03656fe038da7698
tavosalas27 <tavosalas270@gmail.com>
2022-11-10 09:54:38 -0500

    Actualizado el environment para el netify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
 
-    apiPath:"https://prometheus.fintra.co:8443/",
+    apiPath:"https://418a-181-57-229-82.ngrok.io/",
     userName: "APPWEB",
     password: "123456",
 };
