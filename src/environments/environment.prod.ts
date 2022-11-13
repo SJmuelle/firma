3a6b55a593fb37aea32763c5fe740009876e77d1
tavosalas27 <102743153+tavosalas27@users.noreply.github.com>
2022-11-12 22:11:02 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
 
-    apiPath:"https://prometheus.fintra.co:8443/",
+    apiPath:"https://2c01-181-57-229-82.ngrok.io/",
     userName: "APPWEB",
     password: "123456",
 };
