33a7c1881b60b834f19546eaecea1aab82c54acf
tavosalas27 <tavosalas270@gmail.com>
2022-11-17 08:16:41 -0500

    Actualizado el nuevo environment para pruebas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
 
-    apiPath:"https://6f10-181-57-229-82.ngrok.io/",
+    apiPath:"https://5a4c-181-57-229-82.ngrok.io/",
     userName: "APPWEB",
     password: "123456",
 };
